750099bc45325b2b8917d175c7d41bd358b7c73c
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-03-01 11:59:23 -0300

    route users done

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,47 @@
+import db from "../configs/database.js";
 
-export function getUserMe(req, res){
-    return (
-        res.send("OK")
-    ) 
+export async function getUserMe(req, res) {
+    const userId = res.locals.id;
+    try {
+        const userWithVisitCount = await db.query(
+            `
+                SELECT u.id, u.name, SUM(s.views) AS "visitCount"
+                FROM users u
+                JOIN "shortUrls" s
+                ON s."userId" = u.id
+                WHERE u.id = $1
+                GROUP BY u.id;
+            `,[userId]
+        );
+        const shortenedUrls = await db.query(
+            `   SELECT id, code AS "shortUrl", url, views AS "visitCount"
+                FROM "shortUrls"
+                WHERE "userId" = $1;
+            `,[userId]
+        );
+        
+        res.send({...userWithVisitCount.rows[0], shortenedUrls: shortenedUrls.rows});
+    } catch (err) {
+        res.status(500).send(err);
+    }
 }
+
+// {
+//     "id": id do usuário,
+//       "name": nome do usuário,
+//       "visitCount": soma da quantidade de visitas de todos os links do usuário,
+//       "shortenedUrls": [
+//           {
+//               "id": 1,
+//               "shortUrl": "...",
+//               "url": "...",
+//               "visitCount": soma da quantidade de visitas do link
+//           },
+//           {
+//               "id": 2,
+//               "shortUrl": "...",
+//               "url": "...",
+//               "visitCount": soma da quantidade de visitas do link
+//           }
+//       ]
+// }
--- src/routers/UserRouter.js
+++ src/routers/UserRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getUserMe } from "../controllers/UserController.js";
+import authorization from "../middlewares/authorization.js";
 
 
 const UserRouter = Router();
 
-UserRouter.get("users/me", getUserMe);
+UserRouter.get("/users/me", authorization, getUserMe);
 
 export default UserRouter;
