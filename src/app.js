f7c148a07e150c5cdb22314e10b025135108c7eb
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-03-01 12:32:12 -0300

    chore: adjust code for deploy on render 2

--- src/app.js
+++ src/app.js
@@ -8,10 +8,12 @@ import UserRouter from './routers/UserRouter.js';
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use([AuthRouter, RankingRouter, UrlRouter, UserRouter]);
 
-app.listen(process.env.PORT,() => console.log(`Server is running in PORT: ${process.env.PORT}`));
+app.listen(PORT,() => console.log(`Server is running in PORT: ${process.env.PORT}`));
