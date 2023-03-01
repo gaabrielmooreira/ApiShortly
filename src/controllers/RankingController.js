a2d623965494434a228094b129936f2dc26812cc
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-03-01 11:59:02 -0300

    route rank done

--- src/controllers/RankingController.js
+++ src/controllers/RankingController.js
@@ -1,6 +1,21 @@
+import db from "../configs/database.js"
 
-export function getRanking(req, res){
-    return (
-        res.send("OK")
-    ) 
+export async function getRanking(req, res){
+    try{
+        const getRank = await db.query(
+            `
+                SELECT u.id, u.name, COUNT(s.id) AS "linksCount", SUM(s.views) AS "visitCount" 
+                FROM users u
+                JOIN "shortUrls" s
+                ON s."userId" = u.id
+                GROUP BY u.id
+                ORDER BY "visitCount" desc
+                LIMIT 10;
+                `
+        );
+        const rank = getRank.rows;
+        res.send(rank);
+    } catch(err) {
+        res.status(500).send(err);
+    }
 }
